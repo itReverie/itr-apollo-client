b0b75b8f7a3751422d4f7b1fff1f53911cf36d64
itReverie <brenda@itreverie.com>
2018-08-13 12:05:14 -0400

    Adding the subscription inside the mutation.

--- src/components/messageElement/index.js
+++ src/components/messageElement/index.js
@@ -3,40 +3,32 @@ import PropTypes from 'prop-types';
 import './messageElement.css';
 import Star from '../moreActions/star/';
 import gql from 'graphql-tag';
-import { Mutation, Subscription } from 'react-apollo';
+import { Fragment, Mutation, Subscription } from 'react-apollo';
 
 export default class MessageElement extends Component{
 
-    componentDidMount() {
-        this.messageUpdatedSubscription();
-    }
-
-    messageUpdatedSubscription = () => (
-        <Subscription
-          subscription={MESSAGE_UPDATED}>
-          {({ data: { messageUpdated } }) => (
-            console.log(messageUpdated)
-          )}
-        </Subscription>
-      );
-    
     render(){
-        return(
-            <Mutation mutation={UPDATE_MESSAGE} key={this.props.message.id}>
-             {updateMessage => (
-                <div className="message-container">
-                    <div className="text-container">{this.props.message.text}</div> 
-                    <div className="moreActions-container">
-                        <Star active={this.props.message.isFavorite} 
-                            onClick={() => { updateMessage({ variables: { 
-                                                             id: this.props.message.id, 
-                                                             text: this.props.message.text,
-                                                             isFavorite: !this.props.message.isFavorite } });
-                        }}/>
+        return(<Mutation mutation={UPDATE_MESSAGE} key={this.props.message.id}>
+                {updateMessage => (
+                    <div className="message-container">
+                        <div className="text-container">{this.props.message.text}</div> 
+                        <div className="moreActions-container">
+
+                         <Subscription subscription={MESSAGE_UPDATED}
+                                       variables={{ id: this.props.message.id }}>
+                            {() => {
+                                return <Star active={this.props.message.isFavorite} 
+                                            onClick={() => { updateMessage({ variables: { 
+                                                                            id: this.props.message.id, 
+                                                                            text: this.props.message.text,
+                                                                            isFavorite: !this.props.message.isFavorite } });}}/>
+                            }}
+                        </Subscription>
+                        </div>
                     </div>
-                </div>
-              )}
-           </Mutation>);
+                )}
+            </Mutation>
+        );
     }
 }
 
@@ -58,8 +50,8 @@ mutation updateMessage($id: Int!,
 }`;
 
 const MESSAGE_UPDATED = gql`
-   subscription messageUpdated{
-                messageUpdated
+subscription messageUpdated($id: Int!){
+                messageUpdated(id:$id)
 		        { 
                     id
     	            text
